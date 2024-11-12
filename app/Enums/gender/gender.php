bed772a49427b62d7800f1a76866b59877b63aae
trileminhfb <leminhtrisuper@gmail.com>
2024-11-12 15:39:27 +0700

    feat: product database

--- app/Enums/gender/gender.php
+++ app/Enums/gender/gender.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Enums\brand;
+namespace App\Enums\gender;
 
 use BenSampo\Enum\Enum;
 
@@ -11,8 +11,8 @@ use BenSampo\Enum\Enum;
  * @method static static OptionTwo()
  * @method static static OptionThree()
  */
-final class statusBrand extends Enum
+final class gender extends Enum
 {
-    const DISABLE = 0;
-    const ENABLE = 1;
+    const WOMEN = 0;
+    const MEN = 1;
 }
