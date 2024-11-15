53ef5e0a1c8ce942e11edff8edbfa15bfe00fa28
trileminhfb <leminhtrisuper@gmail.com>
2024-11-15 13:06:16 +0700

    user Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,28 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'account',
+        'password',
+        'id_role',
+        'fullName',
+        'address',
+        'birth',
+        'phoneNumber',
         'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
