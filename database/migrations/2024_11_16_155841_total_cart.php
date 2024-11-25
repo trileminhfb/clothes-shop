e0687a1524002990a5e6c9b6c27450711180669a
trileminhfb <leminhtrisuper@gmail.com>
2024-11-25 20:34:58 +0700

    feat: crud total cart

--- database/migrations/2024_11_16_155841_total_cart.php
+++ database/migrations/2024_11_16_155841_total_cart.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\totalCart\statusTotalCart;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,6 +19,7 @@ return new class extends Migration
             $table->date('orderTime');
             $table->string('address');
             $table->string('phoneNumber');
+            $table->integer('status')->default(statusTotalCart::WAITING);
             $table->timestamps();
         });
     }
